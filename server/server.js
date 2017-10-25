0761b9c92c1633f90a95cdb1f304dfb088806f32
Neha Awasthi <neha.awasthi1194@gmail.com>
2017-10-25 10:47:57 +0530

    New user

--- server/server.js
+++ server/server.js
@@ -21,6 +21,20 @@ io.on('connection',(socket)=>{
   //   createAt: 123123
   // });
 
+  //...socket.emit from Admin text Welcome to chat app
+  socket.emit('newMessage',{
+    from:'Admin',
+    text:'Welcome to chat app',
+    createdAt: new Date().getTime()
+  });
+
+  //...socket.broadcast.emit from admin text New user join..
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
@@ -28,6 +42,12 @@ io.on('connection',(socket)=>{
       text: message.text,
       createdAt: new Date().getTime()
   });
+//  socket.broadcast.emit('newMessage', {
+//       from: message.from,
+//       text: message.text,
+//       createdAt: new Date().getTime()
+//     });
+
 });
 
     socket.on('disconnect',()=>{
